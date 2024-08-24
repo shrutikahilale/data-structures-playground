eb9d0b7942780cdd0e1b2983d58d083be9528f20
Shrutika Hilale <102667828+shrutikahilale@users.noreply.github.com>
2024-08-24 07:56:23 +0000

    ll fucntionality implemented

--- src/components/all_playgrounds/ll_playground.jsx
+++ src/components/all_playgrounds/ll_playground.jsx
@@ -1,43 +1,71 @@
 import { useState } from "react";
 import "./css/playground.css";
 import "./css/block.css";
+import "./css/ll.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function LinkedListPlayground() {
   const [linkedList, setLinkedList] = useState([1, 2]);
-  const [value, setValue] = useState("");
-  const [idx, setIdx] = useState("");
+  const [val1, setVal1] = useState("");
+  const [idxToAddAt, setIdxToAddAt] = useState("");
+  const [val2, setVal2] = useState("");
+  const [idxToRemoveFrom, setIdxToRemoveFrom] = useState("");
   const [error, setError] = useState("");
 
-  const numValue = (value) => {
-    const sanitizedValue = value.replace(/[^\d]/g, "");
+  const numValue = (val1) => {
+    const sanitizedValue = val1.replace(/[^\d]/g, "");
     const parsedValue = parseInt(sanitizedValue, 10);
     return isNaN(parsedValue) ? "" : parsedValue;
   };
 
   const insertNodeAtIdx = () => {
-    const val = numValue(value);
-    const index = numValue(idx);
-
-    if (val === "") {
-      setError("Value is NAN");
-    } else if (index === "") {
-      setError("Index is NAN");
-    } else if (index > linkedList.length) {
-      setError(
-        `Cannot insert at invalid index (${index}) is greater than number of elements (${linkedList.length})`
-      );
+    if (val1.trim() === "") {
+      setError("Please enter value");
+    } else if (idxToAddAt.trim() === "") {
+      setError("Please enter index");
     } else {
-      setError("");
-      linkedList.splice(index, 0, val);
-      /**
-       * splice(start, deleteCount, item1, item2, ...):
-start: The index at which to start changing the array. 
-deleteCount: An integer indicating the number of elements to remove. (Here, 0, so no elements are removed.)
-item1, item2, ...: The elements to add to the array, starting at the start index.
-its inplace
-       */
+      const val = numValue(val1.trim());
+      const index = numValue(idxToAddAt.trim());
+
+      if (val === "") {
+        setError("Value is NAN");
+      } else if (index === "") {
+        setError("Index is NAN");
+      } else if (index > linkedList.length) {
+        setError(
+          `Cannot insert at invalid index (${index}) is greater than number of elements (${linkedList.length})`
+        );
+      } else {
+        setError("");
+        const ll = [...linkedList];
+        ll.splice(index, 0, val);
+        setLinkedList(ll);
+
+        setVal1("");
+        setIdxToAddAt("");
+      }
+    }
+  };
+
+  const deleteNodeAtIdx = () => {
+    if (idxToRemoveFrom.trim() === "") {
+      setError("Please enter index");
+    } else {
+      const index = numValue(idxToRemoveFrom.trim());
+      if (index === "") {
+        setError("Index is NAN");
+      } else if (index > linkedList.length) {
+        setError(
+          `Cannot delete from invalid index, (${index}) is greater than number of elements (${linkedList.length})`
+        );
+      } else {
+        setError("");
+        const ll = [...linkedList];
+        ll.splice(index, 1);
+        setLinkedList(ll);
+        setIdxToRemoveFrom("");
+      }
     }
   };
 
@@ -62,21 +90,35 @@ its inplace
       <div className="display-flex  gap-10 w-100per jc-center">
         <input
           type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
+          value={val1}
+          onChange={(e) => setVal1(e.target.value)}
           placeholder="Value"
           className="custom-input"
         />
         <input
           type="text"
-          value={idx}
-          onChange={(e) => setIdx(e.target.value)}
+          value={idxToAddAt}
+          onChange={(e) => setIdxToAddAt(e.target.value)}
           placeholder="Node insertion index"
           className="custom-input"
         />
 
         <button onClick={insertNodeAtIdx} className="positive-op">
           Add
+        </button>
+      </div>
+
+      <div className="display-flex  gap-10 w-100per jc-center">
+        <input
+          type="text"
+          value={idxToRemoveFrom}
+          onChange={(e) => setIdxToRemoveFrom(e.target.value)}
+          placeholder="Node deletion index"
+          className="custom-input"
+        />
+
+        <button onClick={deleteNodeAtIdx} className="negative-op">
+          Delete
         </button>
       </div>
     </div>
